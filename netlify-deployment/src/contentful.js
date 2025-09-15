0c2f6c844da5f6df35e029af430d46b24f1ec36e
avazzades <64379185+avazzades@users.noreply.github.com>
2025-09-14 23:46:25 -0700

    Update contentful.js

--- netlify-deployment/src/contentful.js
+++ netlify-deployment/src/contentful.js
@@ -1,8 +1,8 @@
 import { createClient } from 'contentful';
 
 const client = createClient({
-  space: 'YOUR_SPACE_ID',
-  accessToken: 'YOUR_ACCESS_TOKEN',
+  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
 });
 
 export const getBusinessInfo = async () => {
